665d4dbced15873a5108e3e915404866bd830f1b
Abduhoshim <sultonqulov99@gmail.com>
2022-05-24 15:20:32 +0500

    changed

--- src/modules/video/model.js
+++ src/modules/video/model.js
@@ -1,3 +1,13 @@
 import db from '../../utils/db.js'
 
-export default {}
+const UPDATE_VIDEO = `
+    update videos set video_title = $1 where video_id = $2 and user_id = $3 returning *
+`
+const updateVideo = async(video_title,videoId,userId) =>{
+    const video = await db(UPDATE_VIDEO,video_title,videoId,userId)
+    console.log(video);
+    return video
+}
+export default {
+    updateVideo
+}
--- src/modules/video/resolvers.js
+++ src/modules/video/resolvers.js
@@ -1,3 +1,10 @@
 import model from './model.js'
 
-export default {}
+export default {
+    Mutation: {
+        updateVideo: async(_,{video_title,videoId},{userId})=>{
+            console.log(video_title,videoId);
+            console.log(userId);
+        }
+    }
+}
