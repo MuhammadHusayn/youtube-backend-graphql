eca5c4f20c3ed9d9e9306e9cf5844277c6eab775
Abdulbosit089 <abdulbositsobitov6@gmail.com>
2022-05-24 15:43:41 +0500

    created delete video function

--- src/modules/video/model.js
+++ src/modules/video/model.js
@@ -1,3 +1,14 @@
 import db from '../../utils/db.js'
 
-export default {}
+const DELETE_VIDEO = `
+    DELETE FROM videos WHERE video_id = $1 returning *
+`
+
+async function deleteVideo(video_id) {
+    const video = await db(DELETE_VIDEO, video_id)
+    return video
+}
+
+export default {
+    deleteVideo
+}
--- src/modules/video/resolvers.js
+++ src/modules/video/resolvers.js
@@ -1,3 +1,16 @@
 import model from './model.js'
 
-export default {}
+
+export default {
+    Mutation: {
+        deleteVideo: async (_, { video_id }, { userId } ) => {
+            if (!userId) throw new Error("You can not delete video!")
+            const deletedVideo = await model.deleteVideo(video_id)
+            return {
+                status: 200,
+                message: "The video successfully deleted!",
+                data: deletedVideo[0]
+            }
+        },
+    }
+}
