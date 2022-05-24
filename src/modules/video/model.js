88d3e5d53d4446b6f1c63f485b910153c2985455
Jamshid Maxmudov <jamshid.qwerty@mail.ru>
2022-05-24 15:51:15 +0500

    updated add videos

--- src/modules/video/model.js
+++ src/modules/video/model.js
@@ -1,3 +1,15 @@
 import db from '../../utils/db.js'
 
-export default {}
+const CREATE_VIDEO = `
+    INSERT INTO videos 
+        (user_id, video_title, video_link, video_type) values 
+        ($1, $2, $3, $4)
+`
+
+function createVideo({ userId, video_title, video_link, video_type }) {
+    db(CREATE_VIDEO, userId, video_title, video_link, video_type)
+}
+
+export default {
+    createVideo
+}
