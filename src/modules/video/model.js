361d570ee1309031d0f4d19fcc9dfbf9314508c6
Abdulbosit089 <abdulbositsobitov6@gmail.com>
2022-05-24 16:08:51 +0500

    javvob finished

--- src/modules/video/model.js
+++ src/modules/video/model.js
@@ -1,11 +1,11 @@
 import db from '../../utils/db.js'
 
 const DELETE_VIDEO = `
-    DELETE FROM videos WHERE video_id = $1 returning *
+    update videos set video_deleted_at = $1 where video_id = $2 and user_id = $3 returning *
 `
 
-async function deleteVideo(video_id) {
-    const video = await db(DELETE_VIDEO, video_id)
+async function deleteVideo(video_deleted_at, video_id, user_id) {
+    const video = await db(DELETE_VIDEO, video_deleted_at, video_id, user_id)
     return video
 }
 
--- src/modules/video/resolvers.js
+++ src/modules/video/resolvers.js
@@ -1,11 +1,10 @@
 import model from './model.js'
 
-
 export default {
     Mutation: {
-        deleteVideo: async (_, { video_id }, { userId } ) => {
-            if (!userId) throw new Error("You can not delete video!")
-            const deletedVideo = await model.deleteVideo(video_id)
+        deleteVideo: async (_, { video_id }, { user_id }) => {
+            let video_deleted_at = new Date()
+            const deletedVideo = await model.deleteVideo(video_deleted_at, video_id, user_id)
             return {
                 status: 200,
                 message: "The video successfully deleted!",
